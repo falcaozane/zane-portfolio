93c048fe31dd3e0ccea00e5cff89a9dbaa9ca8aa
Zane Falcao <falcaozane@gmail.com>
2025-08-20 21:38:46 +0530

    skills

---
+++ src/data/skills.ts
@@ -0,0 +1,171 @@
+interface SkillBadgeType {
+  [key: string]: string;
+}
+
+export const skill_badges: SkillBadgeType = {
+  c: "https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=black",
+  cplusplus:
+    "https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white",
+  java: "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white",
+  php: "https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white",
+  python:
+    "https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white",
+  html: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
+  css: "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white",
+  javascript:
+    "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+  typescript:
+    "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+  nodejs:
+    "https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white",
+  go: "https://img.shields.io/badge/Go-00ADD8?style=for-the-badge&logo=go&logoColor=white",
+  dart: "https://img.shields.io/badge/Dart-0175C2?style=for-the-badge&logo=dart&logoColor=white",
+  reactjs:
+    "https://img.shields.io/badge/React.js-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+  nextjs:
+    "https://img.shields.io/badge/Next.js-000000.svg?style=for-the-badge&logo=Next.js&labelColor=000000",
+  mysql:
+    "https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white",
+  postgresql:
+    "https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white",
+  firebase:
+    "https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase",
+  mongodb:
+    "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
+  fastapi:
+    "https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&logo=fastapi",
+  flask:
+    "https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white",
+  flutter:
+    "https://img.shields.io/badge/Flutter-02569B?style=for-the-badge&logo=flutter&logoColor=white",
+  langchain:
+    "https://img.shields.io/badge/LangChain-ffffff?logo=langchain&logoColor=green",
+  tensorflow:
+    "https://img.shields.io/badge/TensorFlow-FF6F00?style=for-the-badge&logo=tensorflow&logoColor=white",
+  scikit:
+    "https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white",
+  pandas:
+    "https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white",
+  numpy:
+    "https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white",
+  matplotlib:
+    "https://img.shields.io/badge/Matplotlib-%23ffffff.svg?style=for-the-badge&logo=Matplotlib&logoColor=black",
+  powerbi:
+    "https://img.shields.io/badge/power_bi-F2C811?style=for-the-badge&logo=powerbi&logoColor=black",
+  django:
+    "https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white",
+  figma:
+    "https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white",
+  androidstudio:
+    "https://img.shields.io/badge/Android_Studio-3DDC84?style=for-the-badge&logo=android-studio&logoColor=white",
+  solidity:
+    "https://img.shields.io/badge/Solidity-%23363636.svg?style=for-the-badge&logo=solidity&logoColor=white",
+  ethersjs:
+    "https://img.shields.io/badge/Ethers.js-3C3C3D?style=for-the-badge&logo=Ethereum&logoColor=white",
+  linux:
+    "https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black",
+  vim: "https://img.shields.io/badge/VIM-%2311AB00.svg?style=for-the-badge&logo=vim&logoColor=white",
+  docker:
+    "https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white",
+  git: "https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white",
+  ipfs: "https://img.shields.io/badge/IPFS-65C2CB.svg?style=for-the-badge&logo=IPFS&logoColor=white",
+  trpc: "https://img.shields.io/badge/tRPC-2596BE.svg?style=for-the-badge&logo=tRPC&logoColor=white",
+  prisma:
+    "https://img.shields.io/badge/Prisma-2D3748.svg?style=for-the-badge&logo=Prisma&logoColor=white",
+  pocketbase:
+    "https://img.shields.io/badge/PocketBase-B8DBE4.svg?style=for-the-badge&logo=PocketBase&logoColor=black",
+  arduino:
+    "https://img.shields.io/badge/Arduino-00979D.svg?style=for-the-badge&logo=Arduino&logoColor=white",
+  electron:
+    "https://img.shields.io/badge/Electron-47848F.svg?style=for-the-badge&logo=Electron&logoColor=white",
+  sqlserver:
+    "https://img.shields.io/badge/Microsoft%20SQL%20Server-CC2927?style=for-the-badge&logo=microsoft%20sql%20server&logoColor=white",
+  kaggle:
+    "https://img.shields.io/badge/Kaggle-035a7d?style=for-the-badge&logo=kaggle&logoColor=white",
+  opencv:
+    "https://img.shields.io/badge/opencv-%23white.svg?style=for-the-badge&logo=opencv&logoColor=white",
+};
+
+export const skill_badges_mono: SkillBadgeType = {
+  c: "https://img.shields.io/badge/C-fff?style=for-the-badge&logo=c&logoColor=black",
+  cplusplus:
+    "https://img.shields.io/badge/C%2B%2B-fff?style=for-the-badge&logo=c%2B%2B&logoColor=black",
+  java: "https://img.shields.io/badge/Java-fff?style=for-the-badge&logo=openjdk&logoColor=black",
+  php: "https://img.shields.io/badge/PHP-fff?style=for-the-badge&logo=php&logoColor=black",
+  python:
+    "https://img.shields.io/badge/Python-fff?style=for-the-badge&logo=python&logoColor=black",
+  html: "https://img.shields.io/badge/HTML5-fff?style=for-the-badge&logo=html5&logoColor=black",
+  css: "https://img.shields.io/badge/CSS3-fff?style=for-the-badge&logo=css3&logoColor=black",
+  javascript:
+    "https://img.shields.io/badge/JavaScript-fff?style=for-the-badge&logo=javascript&logoColor=black",
+  typescript:
+    "https://img.shields.io/badge/TypeScript-fff?style=for-the-badge&logo=typescript&logoColor=black",
+  nodejs:
+    "https://img.shields.io/badge/Node.js-fff?style=for-the-badge&logo=node.js&logoColor=black",
+  go: "https://img.shields.io/badge/Go-fff?style=for-the-badge&logo=go&logoColor=black",
+  dart: "https://img.shields.io/badge/Dart-fff?style=for-the-badge&logo=dart&logoColor=black",
+  reactjs:
+    "https://img.shields.io/badge/React.js-fff?style=for-the-badge&logo=react&logoColor=black",
+  nextjs:
+    "https://img.shields.io/badge/Next.js-fff?style=for-the-badge&logo=Next.js&logoColor=black",
+  mysql:
+    "https://img.shields.io/badge/MySQL-fff?style=for-the-badge&logo=mysql&logoColor=black",
+  postgresql:
+    "https://img.shields.io/badge/PostgreSQL-fff?style=for-the-badge&logo=postgresql&logoColor=black",
+  firebase:
+    "https://img.shields.io/badge/firebase-fff?style=for-the-badge&logo=firebase&logoColor=black",
+  mongodb:
+    "https://img.shields.io/badge/MongoDB-fff?style=for-the-badge&logo=mongodb&logoColor=black",
+  fastapi:
+    "https://img.shields.io/badge/FastAPI-fff?style=for-the-badge&logo=fastapi&logoColor=black",
+  flask:
+    "https://img.shields.io/badge/Flask-fff?style=for-the-badge&logo=flask&logoColor=black",
+  flutter:
+    "https://img.shields.io/badge/Flutter-fff?style=for-the-badge&logo=flutter&logoColor=black",
+  langchain:
+    "https://img.shields.io/badge/LangChain-fff?style=for-the-badge&logo=langchain&logoColor=black",
+  tensorflow:
+    "https://img.shields.io/badge/TensorFlow-fff?style=for-the-badge&logo=tensorflow&logoColor=black",
+  scikit:
+    "https://img.shields.io/badge/scikit-fff?style=for-the-badge&logo=scikit-learn&logoColor=black",
+  pandas:
+    "https://img.shields.io/badge/pandas-fff?style=for-the-badge&logo=pandas&logoColor=black",
+  numpy:
+    "https://img.shields.io/badge/numpy-fff?style=for-the-badge&logo=numpy&logoColor=black",
+  matplotlib:
+    "https://img.shields.io/badge/Matplotlib-fff?style=for-the-badge&logo=Matplotlib&logoColor=black",
+  powerbi:
+    "https://img.shields.io/badge/power_bi-fff?style=for-the-badge&logo=powerbi&logoColor=black",
+  django:
+    "https://img.shields.io/badge/Django-fff?style=for-the-badge&logo=django&logoColor=black",
+  figma:
+    "https://img.shields.io/badge/Figma-fff?style=for-the-badge&logo=figma&logoColor=black",
+  androidstudio:
+    "https://img.shields.io/badge/Android_Studio-fff?style=for-the-badge&logo=android-studio&logoColor=black",
+  solidity:
+    "https://img.shields.io/badge/Solidity-fff?style=for-the-badge&logo=solidity&logoColor=black",
+  ethersjs:
+    "https://img.shields.io/badge/Ethers.js-fff?style=for-the-badge&logo=Ethereum&logoColor=black",
+  linux:
+    "https://img.shields.io/badge/Linux-fff?style=for-the-badge&logo=linux&logoColor=black",
+  vim: "https://img.shields.io/badge/VIM-fff?style=for-the-badge&logo=vim&logoColor=black",
+  docker:
+    "https://img.shields.io/badge/docker-fff?style=for-the-badge&logo=docker&logoColor=black",
+  git: "https://img.shields.io/badge/git-fff?style=for-the-badge&logo=git&logoColor=black",
+  ipfs: "https://img.shields.io/badge/IPFS-fff.svg?style=for-the-badge&logo=IPFS&logoColor=black",
+  trpc: "https://img.shields.io/badge/tRPC-fff.svg?style=for-the-badge&logo=tRPC&logoColor=black",
+  prisma:
+    "https://img.shields.io/badge/Prisma-fff.svg?style=for-the-badge&logo=Prisma&logoColor=black",
+  pocketbase:
+    "https://img.shields.io/badge/PocketBase-fff.svg?style=for-the-badge&logo=PocketBase&logoColor=black",
+  arduino:
+    "https://img.shields.io/badge/Arduino-fff.svg?style=for-the-badge&logo=Arduino&logoColor=black",
+  electron:
+    "https://img.shields.io/badge/Electron-fff.svg?style=for-the-badge&logo=Electron&logoColor=black",
+  sqlserver:
+    "https://img.shields.io/badge/Microsoft%20SQL%20Server-fff?style=for-the-badge&logo=microsoft%20sql%20server&logoColor=black",
+  kaggle:
+    "https://img.shields.io/badge/Kaggle-fff?style=for-the-badge&logo=kaggle&logoColor=black",
+  opencv:
+    "https://img.shields.io/badge/opencv-fff?style=for-the-badge&logo=opencv&logoColor=black",
+};
