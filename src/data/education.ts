c9de09c6f104517e60e8227cc2eb0628259a22bd
falcaozane <falcaozane@gmail.com>
2025-12-12 08:59:24 +0530

    education update

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,7 +1,7 @@
 export const education = [
   {
     institue: "Fr. Conceicao Rodrigues College of Engineering, Mumbai",
-    website: "https://www.capgemini.com/in-en/",
+    website: "http://www.frcrce.ac.in/",
     post: "Bachelor of Engineering in Computer Engineering",
     type: "Degree",
     start: "July 2021",
@@ -13,7 +13,7 @@ export const education = [
   },
   {
     institue: "Thomas Baptista Junior College, Papdy, Vasai",
-    website: "https://arconnet.com/",
+    website: "https://thomasbaptistajc.com/php/home.php",
     post: "HSC - Maharashtra State Board",
     type: "Higher secondary education",
     start: "June 2019",
@@ -22,10 +22,10 @@ export const education = [
     skills: ["Next.js", "SQLite", "Pocketbase", "Recharts"],
   },
   {
-    institue: "St. Anthony's High School, Vasai",
+    institue: "St. Anthony's Convent High School, Vasai",
     image:
       "https://media.licdn.com/dms/image/C4D0BAQFwq94aaHO6Mw/company-logo_200_200/0/1657880937495?e=1695254400&v=beta&t=uc1-RlDWy8O5JaJtK5zsws3yJvgW3kJWvdlDn2hy_YE",
-    website: "",
+    website: "https://share.google/IUwNCvJx979jeoZMO",
     post: "SSC - Maharashtra State Board",
     type: "Secondary education",
     start: "June 2006",
